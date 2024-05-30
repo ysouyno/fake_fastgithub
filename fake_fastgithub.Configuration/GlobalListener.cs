57b67f2de2c25620d791257fe407d44c05d0883d
ysouyno <ysouyno@163.com>
2024-05-30 17:02:40 +0800

    project of fake_fastgithub.Configuration added

---
+++ fake_fastgithub.Configuration/GlobalListener.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+
+namespace fake_fastgithub.Configuration
+{
+    public static class GlobalListener
+    {
+        private static readonly IPGlobalProperties global = IPGlobalProperties.GetIPGlobalProperties();
+        private static readonly HashSet<int> tcp_listen_ports = GetListenPorts(global.GetActiveTcpListeners);
+
+        private static HashSet<int> GetListenPorts(Func<IPEndPoint[]> func)
+        {
+            var hash_set = new HashSet<int>();
+            try
+            {
+                foreach (var endpoint in func())
+                {
+                    hash_set.Add(endpoint.Port);
+                }
+            }
+            catch (Exception) { }
+
+            return hash_set;
+        }
+
+        private static int GetAvailablePort(Func<int, bool> can_func, int min_port)
+        {
+            for (var port = min_port; port < IPEndPoint.MaxPort; port++)
+            {
+                if (can_func(port) == true)
+                {
+                    return port;
+                }
+            }
+            throw new FakeFastGithubException("当前无可用的端口");
+        }
+
+        public static bool CanListenTcp(int port)
+        {
+            return tcp_listen_ports.Contains(port) == false;
+        }
+
+        public static int GetAvailableTcpPort(int min_port)
+        {
+            return GetAvailablePort(CanListenTcp, min_port);
+        }
+
+        public static int HttpsPort { get; } = OperatingSystem.IsWindows() ? GetAvailableTcpPort(443) : GetAvailableTcpPort(38443);
+    }
+}
--- fake_fastgithub.HttpServer/KestrelServerExtensions.cs
+++ fake_fastgithub.HttpServer/KestrelServerExtensions.cs
@@ -1,14 +1,37 @@
 ﻿using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace fake_fastgithub
 {
     public static class KestrelServerExtensions
     {
+        private static ILogger GetLogger(this KestrelServerOptions kestrel)
+        {
+            var loggerFactory = kestrel.ApplicationServices.GetRequiredService<ILoggerFactory>();
+            return loggerFactory.CreateLogger($"{nameof(fake_fastgithub)}.HttpServer");
+        }
+
         public static void NoLimit(this KestrelServerOptions kestrel)
         {
             kestrel.Limits.MaxRequestBodySize = null;
             kestrel.Limits.MinResponseDataRate = null;
             kestrel.Limits.MinRequestBodyDataRate = null;
+        }
+
+        private static void ListenHttpsReverseProxy(this KestrelServerOptions kestrel)
+        {
+            var https_port = 443; // GlobalListener.HttpsPort;
+            kestrel.ListenLocalhost(https_port, listen =>
+            {
+                // TODO
+            });
+
+            if (OperatingSystem.IsWindows())
+            {
+                var logger = kestrel.GetLogger();
+                logger.LogInformation($"已监听 https://localhost:{https_port}，https 反向代理服务启动完成");
+            }
         }
     }
 }
