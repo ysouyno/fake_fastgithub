e1f3e42c2976e9d6c64719887ed33f385ae2dd15
ysouyno <ysouyno@163.com>
2024-06-29 17:36:56 +0800

    for func: AddHttpForwarder

--- fake_fastgithub/ReverseProxyServiceCollectionExtensions.cs
+++ fake_fastgithub/ReverseProxyServiceCollectionExtensions.cs
@@ -4,7 +4,10 @@
     {
         public static IServiceCollection AddReverseProxy(this IServiceCollection services)
         {
-            return services.AddSingleton<CertService>();
+            return services
+                .AddMemoryCache()
+                .AddHttpForwarder()
+                .AddSingleton<CertService>();
         }
     }
 }
